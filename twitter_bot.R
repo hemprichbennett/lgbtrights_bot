6f1e9b6ef3c7a54c5301f76585684a5dfe762738
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-02 21:15:21 +0100

    Added formatting so that countries wouldnt show up like 'New Caledonia (overseas...' rather than 'New Caledonia(overseas...' etc

--- twitter_bot.R
+++ twitter_bot.R
@@ -56,7 +56,8 @@ while(A==FALSE){
   }
   rights_name <- gsub('\\.', ' ', colnames(df)[column_chosen])
   country <- df[row_chosen, 1]
-  
+  country <- gsub('\\(', ' \\(', country)
+  country <- gsub('\\\n', ' ', country)
   outstring <- paste(country, '. ', rights_name, ': ', string, sep = '', ' #LGBTQ #equality')
   
   if(nchar(outstring)<=240){
